707e173592cd5eb0f20e5625a1ebcb086bd258b0
heikoliedtke <heiko.liedtke@dimensiondata.com>
2018-12-17 13:22:08 +0100

    Added metadata for mysql

--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ variable "credentials" {}
 variable "project" {}
 variable "region" {}
 variable "startup_script" {}
+variable "mysql_root_password" {}
+variable "mysql_database" {}
+variable "mysql_user" {}
+variable "mysql_password" {}
+
  
 
 
